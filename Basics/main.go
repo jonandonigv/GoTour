51e9a165a2854f02b8b1493c5ee2c54798d3eaef
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-02-02 22:05:05 +0100

    +

--- Basics/main.go
+++ Basics/main.go
@@ -5,6 +5,7 @@ import "fmt"
 // fucking cheater
 // you fucking dum dum
 // Okay I need to finish the go tour but also check what to build
+// TODO: Tonight I need to plan at leat three projects and start one of those
 func main() {
 	fmt.Println("Basics")
 }
