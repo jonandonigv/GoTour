696397144e2af73836b365eb1da2432e2dee25ae
Jon Andoni Galdos <jonandonigv@gmail.com>
2024-02-05 16:42:55 +0100

    basic

--- Basics/main.go
+++ Basics/main.go
@@ -1,9 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+	"math/cmplx"
+	"math/rand"
+)
 
-// Okay I need to finish the go tour but also check what to build
-// TODO: Tonight I need to plan at leat three projects and start one of those
+func add(x, y int) int {
+	return x + y
+}
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
-	fmt.Println("Basics")
+	fmt.Println("My favorite number is", rand.Intn(10))
+	fmt.Println(math.Pi)
+	fmt.Println(add(42, 13))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
+
+	var c, python, java bool
+	var i int
+	fmt.Println(c, python, java, i)
+
+	var (
+		ToBe   bool       = false
+		MaxInt uint64     = 1<<64 - 1
+		z      complex128 = cmplx.Sqrt(-5 + 12i)
+	)
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
 }
